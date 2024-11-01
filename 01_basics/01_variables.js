1e0190c062a69e7eedaf43f3f1441ab33e36c76f
Disha Agrawal <122633438+agarwaldisha7616@users.noreply.github.com>
2024-11-01 16:28:07 +0000

    .

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId=144401
+let accountEmail="john@gmail.com"
+var accountPassword="12345"
+accountCity="Jaipur"
+
+
+accountEmail="jam@gmail.com"
+accountPassword="121212"
+accountCity="Bengaluru"
+
+console.log(accountId)
+/*
+Prefer not to use var 
+because of issue in block scope and functional scope
+*/
+console.table([accountId,accountEmail, accountPassword,accountCity])
