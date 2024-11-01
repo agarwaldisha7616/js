dc0069fe993d481a419efdc650db4be6df91063d
Disha Agrawal <122633438+agarwaldisha7616@users.noreply.github.com>
2024-11-01 16:29:28 +0000

    .

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -13,4 +13,4 @@ console.log(accountId)
 Prefer not to use var 
 because of issue in block scope and functional scope
 */
-console.table([accountId,accountEmail, accountPassword,accountCity])
+console.table([accountId, accountEmail, accountPassword, accountCity])
