9d5ce069529f8c3652690fed4361641a3dd7bdcd
Disha Agrawal <122633438+agarwaldisha7616@users.noreply.github.com>
2024-11-02 15:58:22 +0000

    .

--- 01_basics/03_ConversionOperation.js
+++ 01_basics/03_ConversionOperation.js
@@ -62,3 +62,13 @@ console.log(1+2+"2");   //32
 
 console.log(true);      //true  
 console.log(+true);     //1
+console.log(+"");       //0
+
+
+let num1,num2,num3;
+num1=num2=num3=2+2     //num1=4,num2=4,num3=4
+
+
+let gameCounter=100;
+gameCounter++;
+console.log(gameCounter);   //101
