9b7ef6e5492b6afe327994b93156d9c555f9473b
Disha Agrawal <122633438+agarwaldisha7616@users.noreply.github.com>
2024-11-04 16:46:50 +0000

    ""

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -11,6 +11,20 @@
 // undefined => 
 // symbol => unique
 
+// 1) Primitive Datatypes
+// Number => number
+// String  => string
+// Boolean  => boolean
+// null  => object
+// undefined  =>  undefined
+// Symbol  =>  symbol
+// BigInt  =>  bigint
+
+// 2) Non-primitive Datatypes
+// Arrays  =>  object
+// Function  =>  function
+// Object  =>  object
+
 
 
 // object
